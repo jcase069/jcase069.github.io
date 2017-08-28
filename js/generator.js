a0571c88db29802aeb6a03daa5490e4574bc4a9a
Dan Hedges <daniel.a.hedges@gmail.com>
2017-08-28 10:20:45 -0500

    do not use duplicates

--- js/generator.js
+++ js/generator.js
@@ -4,19 +4,23 @@ function randomParagraphSize(avgParagraphSize) {
   return Math.floor(Math.random() * (avgParagraphSize * 2 - 1)) + 1;
 }
 
-function randomLyric() {
-  return lyrics[Math.floor(Math.random() * lyrics.length)];
+function randomLyric(lyrics) {
+  if (lyrics.length == 0) {
+    return "";
+  }
+  var index = Math.floor(Math.random() * lyrics.length);
+  return lyrics.splice(index, 1)[0];
 }
 
-function generateParagraph(avgParagraphSize, options) {
+function generateParagraph(lyrics, avgParagraphSize, options) {
   let paragraphSize = randomParagraphSize(avgParagraphSize);
   let text = '';
   if (options.paragraphPre) {
     text = text + options.paragraphPre;
   }
-  text += randomLyric();
+  text += randomLyric(lyrics);
   for (let i=1; i<paragraphSize; ++i) {
-    text += ' ' + randomLyric();
+    text += ' ' + randomLyric(lyrics);
   }
   if (options.paragraphPost) {
     text += options.paragraphPost;
@@ -24,17 +28,17 @@ function generateParagraph(avgParagraphSize, options) {
   return text;
 }
 
-function generateText(paragraphs, avgParagraphSize, options) {
+function generateText(lyrics, paragraphs, avgParagraphSize, options) {
   let text = '';
   if (options.pre) {
     text += options.pre;
   }
-  text += generateParagraph(avgParagraphSize, options);
+  text += generateParagraph(lyrics, avgParagraphSize, options);
   for (let i=1; i<paragraphs; ++i) {
     if (options.paragraphBetween) {
       text += options.paragraphBetween;
     }
-    text += generateParagraph(avgParagraphSize, options);
+    text += generateParagraph(lyrics, avgParagraphSize, options);
   }
   if (options.post) {
     text += options.post;
@@ -42,12 +46,16 @@ function generateText(paragraphs, avgParagraphSize, options) {
   return text;
 }
 
+function getLyricCopy() {
+  return lyrics.slice();
+}
+
 function generateHtml(paragraphs, avgParagraphSize) {
   const options = {
     paragraphPre: '<p>',
     paragraphPost: '</p>\n',
   }
-  return generateText(paragraphs, avgParagraphSize, options);
+  return generateText(getLyricCopy(), paragraphs, avgParagraphSize, options);
 }
 
 function generatePreformatted(paragraphs, avgParagraphSize) {
@@ -56,5 +64,5 @@ function generatePreformatted(paragraphs, avgParagraphSize) {
     pre: '<pre>',
     post: '<post>',
   };
-  return generateText(paragraphs, avgParagraphSize, options);
+  return generateText(getLyricCopy(), paragraphs, avgParagraphSize, options);
 }
